e7744a58ea792f63fcd506d1239f09baba5950e1
Italo Matos <italomatossouza@hotmail.com>
2024-06-06 15:47:50 -0300

    [TASK] created category header

--- lib/modules/home/presentation/widgets/atom/atom_category_header.dart
+++ lib/modules/home/presentation/widgets/atom/atom_category_header.dart
@@ -1,9 +1,11 @@
-import 'package:b2b_hortifruti/shared/presentation/theme/colors.dart';
+import 'package:b2b_hortifruti/modules/home/presentation/extensions/box_decoration.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../services/responsive_service.dart';
+import '../../styles/atom_shadow_style.dart';
 
 class AtomCategoryHeader extends StatefulWidget {
   final Widget child;
@@ -11,32 +13,45 @@ class AtomCategoryHeader extends StatefulWidget {
   final double? contentWidth;
   final double? contentHeight;
   final Color backgroundColor;
-  const AtomCategoryHeader(
-      {super.key,
-      required this.child,
-      required this.videoAsset,
-      this.contentWidth,
-      this.contentHeight, required this.backgroundColor});
+
+  const AtomCategoryHeader({
+    super.key,
+    required this.child,
+    required this.videoAsset,
+    this.contentWidth,
+    this.contentHeight,
+    required this.backgroundColor,
+  });
 
   @override
   State<AtomCategoryHeader> createState() => _AtomCategoryHeaderState();
 }
 
 class _AtomCategoryHeaderState extends State<AtomCategoryHeader> {
-  VideoPlayerController? _controller;
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoFuture;
 
   @override
   void initState() {
     super.initState();
+    _controller = VideoPlayerController.asset(
+      widget.videoAsset,
+      videoPlayerOptions: !kIsWeb ? VideoPlayerOptions(mixWithOthers: true) : null,
+    );
+    _initializeVideoFuture = _initializeVideo();
+  }
 
-    Future.microtask(() async {
-      _controller = VideoPlayerController.asset(widget.videoAsset);
-      await _controller!.setLooping(true);
-      await _controller!.initialize().then((_) {
-        _controller!.play();
-        setState(() {});
-      });
-    });
+  Future<void> _initializeVideo() async {
+    await _controller.initialize();
+    _controller.setVolume(0);
+    _controller.setLooping(true);
+    await _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -44,53 +59,67 @@ class _AtomCategoryHeaderState extends State<AtomCategoryHeader> {
     final decoration = BoxDecoration(
       color: widget.backgroundColor,
       borderRadius: BorderRadius.circular(32),
-      boxShadow: const [
-        BoxShadow(
-          blurRadius: 10,
-          offset: Offset(0, 4),
-          color: AppColors.shadow2,
-        )
-      ],
     );
 
-    return _controller == null
-        ? Shimmer.fromColors(
-            baseColor: Colors.black12,
-            highlightColor: Colors.white,
-            child: Container(
-              height: ResponsiveService.isMobile(context) ? 256 : 272,
-              width: MediaQuery.of(context).size.width,
-              decoration: decoration,
-            ),
-          )
-        : Container(
-            decoration: decoration,
-            height: ResponsiveService.isMobile(context) ? 256 : 272,
-            width: MediaQuery.of(context).size.width,
-            child: AspectRatio(
-              aspectRatio: _controller!.value.aspectRatio,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(32),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    FittedBox(
-                      fit: BoxFit.cover,
-                      child: SizedBox(
-                        width: _controller!.value.size.width,
-                        height: _controller!.value.size.height,
-                        child: VideoPlayer(_controller!),
-                      ),
-                    ),
-                    SizedBox(
-                      height: widget.contentHeight,
-                      width: widget.contentWidth,
-                      child: Center(child: widget.child),
-                    ),
-                  ],
+    return FutureBuilder<void>(
+      future: _initializeVideoFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return _buildVideoPlayer(decoration, context);
+        } else {
+          return _buildShimmer(decoration, context);
+        }
+      },
+    );
+  }
+
+  Widget _buildVideoPlayer(BoxDecoration decoration, BuildContext context) {
+    return Container(
+      decoration: decoration.withBoxShadow(AtomShadowStyle.categoryHeader),
+      height: ResponsiveService.isMobile(context) ? 224 : 272,
+      width: MediaQuery.of(context).size.width,
+      child: AspectRatio(
+        aspectRatio: _controller.value.aspectRatio,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(32),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: _controller.value.size.width,
+                  height: _controller.value.size.height,
+                  child: VideoPlayer(_controller),
                 ),
               ),
-            ),
-          );
+              SizedBox(
+                height: widget.contentHeight,
+                width: widget.contentWidth,
+                child: Center(child: widget.child),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildShimmer(BoxDecoration decoration, BuildContext context) {
+    return Container(
+      decoration: decoration.copyWith(
+        boxShadow: AtomShadowStyle.categoryHeader,
+        color: Colors.white,
+      ),
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.grey.shade100,
+        child: Container(
+          height: ResponsiveService.isMobile(context) ? 256 : 272,
+          width: MediaQuery.of(context).size.width,
+          decoration: decoration,
+        ),
+      ),
+    );
   }
 }
--- lib/modules/home/presentation/widgets/atom/atom_category_header_button.dart
+++ lib/modules/home/presentation/widgets/atom/atom_category_header_button.dart
@@ -1,4 +1,5 @@
-import 'package:b2b_hortifruti/shared/presentation/theme/colors.dart';
+import 'package:b2b_hortifruti/modules/home/presentation/services/responsive_service.dart';
+import 'package:b2b_hortifruti/modules/home/presentation/styles/atom_shadow_style.dart';
 import 'package:b2b_hortifruti/shared/presentation/theme/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
@@ -23,25 +24,22 @@ class AtomCategoryHeaderButton extends StatelessWidget {
     return visible
         ? InkWell(
             child: Container(
-              height: 112,
-              width: 256,
+              height: ResponsiveService.isMobile(context) ? 96 : 112,
+              width: ResponsiveService.isMobile(context) ? 224 : 256,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
                 color: color,
-                boxShadow: const [
-                  BoxShadow(
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
-                    color: AppColors.shadow3,
-                  ),
-                ],
+                boxShadow: AtomShadowStyle.categoryHeaderButton,
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 16.0),
-                    child: Text(title, style: AppTextStyle.categoryHeaderButton),
+                    child: Text(title,
+                        style: AppTextStyle.categoryHeaderButton.copyWith(
+                          fontSize: ResponsiveService.isMobile(context) ? 18 : 20,
+                        )),
                   ),
                   ClipRRect(
                       borderRadius: const BorderRadius.only(
@@ -61,8 +59,8 @@ class AtomCategoryHeaderButton extends StatelessWidget {
               borderRadius: BorderRadius.circular(16),
             ),
             child: Shimmer.fromColors(
-              baseColor: Colors.black12,
-              highlightColor: Colors.white70,
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
               child: Container(
                 height: 112,
                 width: 256,
